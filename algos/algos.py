cd617fe49ba60b17785f16a929d959b8cb83625f
Joshua Kerber <jkerber14@comcast.net>
2020-04-19 03:29:31 -0400

    update sleep interval to 30 mins, price target to +$1, and allow ETH,LTC

--- algos/algos.py
+++ algos/algos.py
@@ -1,3 +1,3 @@
 def crunchNumbers(crypto):
     """Crunch the numbers and determine if buying this cryptocurrency is a smart move."""
-    return True, 1.0, crypto.buyPrice + 0.01  # for now- always buy 1 with +1 cent price target
+    return True, 1.0, crypto.buyPrice + 1.0  # for now- always buy 1 with +1 dollar price target
--- constants.py
+++ constants.py
@@ -3,9 +3,9 @@ import os
 
 # kraken API constants
 KRAKEN_API_BASE = "https://api.kraken.com/0/"
-KRAKEN_CRYPTO_TICKERS = {"BTC": "XBT",}
-                         # "ETH": "ETH",
-                         # "LTC": "LTC"}
+KRAKEN_CRYPTO_TICKERS = {"BTC": "XBT",
+                         "ETH": "ETH",
+                         "LTC": "LTC"}
 KRAKEN_KEY_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                "kraken",
                                "keys",
@@ -19,8 +19,8 @@ IS_SIMULATION = False
 SIM_ACCOUNT_BALANCE = 1000.0
 SIM_BALANCES = {ticker: 0 for ticker in KRAKEN_CRYPTO_TICKERS.keys()}
 
-# delay between requests
-SLEEP_INTERVAL = 1.5
+# delay between requests in seconds
+SLEEP_INTERVAL = 1800
 
 # cryptos bitbot is allowed to trade with
 SUPPORTED_CRYPTOS = KRAKEN_CRYPTO_TICKERS.keys()
