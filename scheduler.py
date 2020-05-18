61452fcdbb2895d4d644ae97125cb876726f8415
Joshua Kerber <jkerber14@comcast.net>
2020-05-17 23:05:17 -0400

    scheduler script

---
+++ scheduler.py
@@ -0,0 +1,11 @@
+"""Script to call app endpoints via the Heroku scheduler."""
+import app
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        raise RuntimeError("function name must be provided")
+    methodName = sys.argv[1]
+    method = getattr(app, methodName)
+    args = sys.argv[2:]
+    method(*args)
