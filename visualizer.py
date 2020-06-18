f7c462b543affe9339975a2741b09898c8478ed1
Joshua Kerber <jkerber14@comcast.net>
2020-06-17 19:28:17 -0700

    lighten visualization colors + 2-day ticks

--- visualizer.py
+++ visualizer.py
@@ -7,8 +7,8 @@ from matplotlib import pyplot
 from algos import mean_reversion
 from algos import linear_regression
 
-SECONDS_IN_DAY = 3600 * 24
 CHROME_IMAGE_BACKGROUND_COLOR_HEX = "#0e0e0e"
+SECONDS_IN_DAY = 3600 * 24
 
 # ensure Flask doesn't try to create GUI windows
 matplotlib.use("Agg")
@@ -40,21 +40,18 @@ def visualize(ticker, currentPrices, priceHistory):
     pyplot.title("%s History" % name)
     pyplot.ylabel("Price ($)")
     pyplot.xlabel("Time (days)")
-    pyplot.grid(color="silver", linestyle="--", linewidth=0.5, alpha=0.5)
+    pyplot.grid(color="silver", linestyle="--", linewidth=0.5, alpha=0.35)
 
     # plot price history with VWAP and trend line
-    pyplot.plot(regression.timestamps, regression.prices, linewidth=3, label=("price ($%.3f)" % currentPrice))
-    pyplot.plot(regression.timestamps, regression.trend, color="red", linewidth=1.25, linestyle="--", label="price trend")
-    pyplot.plot(regression.timestamps, meanReversion.vwaps, color="darkorange", label="VWAP ($%.3f)" % currentVWAP)
-
-    # plot present day
-    currentTimestamp = datetime.datetime.utcnow().timestamp()
-    pyplot.axvline(x=currentTimestamp, color="grey", label="now")
+    pyplot.plot(regression.timestamps, regression.prices, color="cornflowerblue", linewidth=1, label=("price ($%.3f)" % currentPrice))
+    pyplot.plot(regression.timestamps, regression.trend, color="red", linewidth=1, linestyle="--", label="price trend")
+    pyplot.plot(regression.timestamps, meanReversion.vwaps, color="orange", linewidth=1, label="VWAP ($%.3f)" % currentVWAP)
 
     # set x-axis ticks to incrementing days
     labels = []
     startingTimestamp = regression.timestamps[0][0]
-    ticks = numpy.arange(startingTimestamp, currentTimestamp, step=SECONDS_IN_DAY)
+    currentTimestamp = datetime.datetime.utcnow().timestamp()
+    ticks = numpy.arange(startingTimestamp, currentTimestamp, step=(SECONDS_IN_DAY * 2))
     for tickTimestamp in ticks:
         daysFromStart = (tickTimestamp - startingTimestamp) / SECONDS_IN_DAY
         labels.append("%i" % daysFromStart)
@@ -62,7 +59,7 @@ def visualize(ticker, currentPrices, priceHistory):
     # add tick for now and display labels
     ticks = numpy.append(ticks, currentTimestamp)
     currentDaysFromStart = (currentTimestamp - startingTimestamp) / SECONDS_IN_DAY
-    labels.append("%.2f" % currentDaysFromStart)
+    labels.append("%.1f (now)" % currentDaysFromStart)
     pyplot.xticks(ticks, labels)
 
     # generate visualization
